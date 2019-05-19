6fd2d7f2f5f281ff816f80f925791a1ccd70fc89
Katukuri <katukuri-sahithi@dxc.com>
2019-05-19 15:54:42 +0530

    modifications are done

--- src/main/java/com/dxc/git/controller/SimpleController.java
+++ src/main/java/com/dxc/git/controller/SimpleController.java
@@ -11,12 +11,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author gino
  */
 @Controller
+@RestController
+@RequestMapping("/welcome")
 public class SimpleController {
 
     @Value("${spring.application.name}")
